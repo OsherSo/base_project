2566b3bf9baf461ce698bc73c2e91c260f92f708
Osher <solimani.osher@gmail.com>
2025-02-02 00:42:47 +0200

    Add Login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,21 @@
+import { Link, Form } from "react-router-dom";
+
+import { FormRow, Logo, SubmitBtn } from "../components";
+
 const Login = () => {
-  return <h1 className="text-3xl font-bold underline">Login</h1>;
+  return (
+    <Form method="post">
+      <Logo />
+      <h4>Login</h4>
+      <FormRow type="email" name="email" labelText="Email" />
+      <FormRow type="password" name="password" labelText="Password" />
+      <SubmitBtn />
+      <p>
+        Not a member yet?
+        <Link to="/register">Register</Link>
+      </p>
+    </Form>
+  );
 };
 
 export default Login;
